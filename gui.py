b426b3cd5a0fcf545861fb87779aefddf352e386
PSM1729 <sabitamishra1729@gmail.com>
2021-10-12 22:30:43 +0530

    Some functions added

--- gui.py
+++ gui.py
@@ -41,40 +41,58 @@ label4.place(x=345,y=5)
 
 tab_control.pack(expand=1, fill="both")
 
-#Define functions
-
-
-
 ############################################# Main Summarizer menu widgets##############################################
+
+###Functions for main menu
+def clear_text_main():
+    entry.delete('1.0',END)
+
+def clear_textbox():
+    tab1_display.delete('1.0', END)
+
+#def basic_summarizer():
+
+
 
 label_m=Label(tab1, text='ENTER THE TEXT', pady=5)
 label_m.place(x=359,y=40)
 entry=ScrolledText(tab1,height=13, width=93, padx=6, pady=5,state='normal')
 entry.place(x=10,y=75)
 
+#Summary TextBox
+tab1_display = Text(tab1,height=19, width=95)
+tab1_display.place(x=10, y=350)
+
 #Buttons
-button1=Button(tab1,text="Reset", width=12,bg='#03A9F4',fg='#000')
+button1=Button(tab1,text="Reset",command=lambda: [clear_textbox(),clear_text_main()], width=12,bg='#03A9F4',fg='#000')
 button1.place(x=270,y=310)
 
 button2=Button(tab1,text="Summarize",width=12,bg='#ced',fg='#000')
 button2.place(x=450,y=310)
 
-#Summary TextBox
-tab1_display = Text(tab1,height=19, width=95).place(x=10, y=350)
-
-########################################################################################################################
-
-
 
 ################################################## File processing Summarizer##########################################
+
+###Function for file processing
+def clear_text_file():
+    displayed_file.delete('1.0',END)
+
+def clear_textbox2():
+    tab2_display_text.delete('1.0', END)
+
+def open_files():
+	file1 = tk.filedialog.askopenfilename(filetypes=(("Text Files",".txt"),("All files","*")))
+	read_text = open(file1).read()
+	displayed_file.insert(tk.END,read_text)
+
 
 displayed_file = ScrolledText(tab2,height=15,width=93)
 displayed_file.grid(row=2,column=0, columnspan=3,padx=5,pady=3)
 
-b0=Button(tab2,text="Open File", width=12,bg='#c5cae9')
+b0=Button(tab2,text="Open File",command=open_files, width=12,bg='#c5cae9')
 b0.grid(row=3,column=0,padx=10,pady=10)
 
-b1=Button(tab2,text="Reset ", width=12,bg="#b9f6ca")
+b1=Button(tab2,text="Reset",command=lambda: [clear_textbox2(),clear_text_file()], width=12,bg="#b9f6ca")
 b1.grid(row=3,column=1,padx=10,pady=10)
 
 b2=Button(tab2,text="Summarize", width=12,bg='#F5EABA',fg='#000')
@@ -83,7 +101,6 @@ b2.grid(row=3,column=2,padx=10,pady=10)
 tab2_display_text = ScrolledText(tab2,height=20,width=93)
 tab2_display_text.grid(row=7,column=0, columnspan=3,padx=5,pady=5)
 
-#######################################################################################################################
 
 ################################################## URL BASED SUMMARIZER#################################################
 
@@ -110,7 +127,7 @@ button2_URL.place(x=450,y=330)
 #Displaying the result
 tab3_display_text = ScrolledText(tab3,height=18,width=95)
 tab3_display_text.place(x=5,y=370)
-#######################################################################################################################
+
 
 ################################################## COMPARING SUMMARIZER#################################################
 
@@ -132,7 +149,7 @@ button3_cmp.place(x=600,y=310)
 #Summary TextBox
 tab1_display_cmp = Text(tab4,height=19, width=95).place(x=10, y=350)
 
-########################################################################################################################
+
 root.mainloop()
 
 
